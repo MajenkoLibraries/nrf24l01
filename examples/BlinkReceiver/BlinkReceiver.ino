f6d49c4b37eb354b0367b77564ad51f91a25f802
Matt Jenkins <matt@majenko.co.uk>
2014-12-22 21:16:56 +0000

    Added some useful comments to the examples

--- examples/BlinkReceiver/BlinkReceiver.ino
+++ examples/BlinkReceiver/BlinkReceiver.ino
@@ -2,9 +2,11 @@
 
 DSPI0 spi;
 
+// SPI port DSPI0, NCS is pin 10, CE is pin 9 and the interrupt is INT1
 nrf24l01 rf(spi, 10, 9, 1);
 
 void setup() {
+    // Join mesh number 1, with my address 10,0,0,2 on channel 0.
 	rf.begin(1, 10,0,0,2, 0);
 	pinMode(PIN_LED1, OUTPUT);
 }
--- examples/BlinkSender/BlinkSender.ino
+++ examples/BlinkSender/BlinkSender.ino
@@ -2,16 +2,22 @@
 
 DSPI0 spi;
 
+// SPI port DSPI0, NCS is pin 10, CE is pin 9 and the interrupt is INT1
 nrf24l01 rf(spi, 10, 9, 1);
 
 void setup() {
 	spi.begin();
+    // Join mesh number 1, with my address 10,0,0,1 on channel 0.
 	rf.begin(1, 10,0,0,1, 0);
 }
 
 void loop() {
+    // Packets are 32 bytes in size.
 	uint8_t buffer[32];
+
+    // We want to talk to 10,0,0,2 on mesh 1.
 	uint8_t dest[5] = {1, 10,0,0,2 };
+
 	buffer[0] = 1;
 	rf.queuePacket(dest, buffer);
 	delay(100);
